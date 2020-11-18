e95401f4e49944b17b7fe5bd3ac9377205a6a851
Mihail <kolesnichenkomihail@gmail.com>
2020-11-18 19:16:13 +0300

    petrovich

---
+++ js/backend.js
@@ -0,0 +1,64 @@
+'use strict';
+
+(function () {
+
+  // JSON адрес
+  var URL = '/';
+  // JSON файл
+  var JSON_CONTAINER = 'products.json';
+
+  var STATUS_CODE_OK = 200;
+  var RESPONSE_TYPE_JSON = 'json';
+  var XHR_TIMEOUT = 10000;
+  var METHOD_GET = 'GET';
+  var METHOD_POST = 'post';
+
+  var renderError = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red; position: absolute; left: 0; right: 0; fontSize: 30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  var request = function (method, url, onSuccess, onError, data) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = RESPONSE_TYPE_JSON;
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === STATUS_CODE_OK) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = XHR_TIMEOUT;
+
+    xhr.open(method, url);
+    xhr.send(data);
+  };
+
+  var save = function (onSuccess, onError, data) {
+    request(METHOD_POST, URL, onSuccess, onError, data);
+  };
+
+  var load = function (onSuccess, onError) {
+    request(METHOD_GET, URL + JSON_CONTAINER, onSuccess, onError);
+  };
+
+  window.backend = {
+    load: load,
+    upload: save,
+    renderError: renderError
+  };
+})();
--- js/script.js
+++ js/script.js
@@ -1,44 +1,142 @@
 'use strict';
 
-try {
-  var navHeader = document.querySelector('.nav');
-  var navToggle = document.querySelector('.toggle__button');
-  var navLinksList = navHeader.querySelectorAll('.nav__link');
+(function () {
+  var productArea = document.querySelector('.product__area');
+  var tamlateContainer = document.querySelector('#products_section');
+  var tamlate = tamlateContainer.querySelector('.products_page');
 
-  navHeader.classList.remove('nav--no-js');
+  // копия карточки
+  var getTamplate = function () {
+    var card = tamlate.cloneNode(true);
 
-  var onLinkClick = function () {
-    if (navHeader.classList.contains('nav--active')) {
-      navHeader.classList.remove('nav--active');
-      navToggle.classList.remove('toggle__button--cross');
+    return card;
+  };
 
-      getListener();
+  // замена имени файла
+  var changeFileName = function (elm) {
+    var splitName = elm.split('.');
+
+    var rename = splitName[0] + '.' + splitName[1] + '_220x220_1' + '.' + splitName[2];
+
+    return rename;
+  };
+
+  // смена точки на запятую
+  var setPrice = function (elm) {
+    var splitPrice = elm.toString().split('.');
+
+    if (splitPrice.length > 1) {
+      return splitPrice[0] + ',' + splitPrice[1];
+    }
+    return elm;
+  };
+
+  // добовление свойств
+  var specifyElement = function (obj) {
+    var productId = obj.productId;
+    var imageUrl = obj.primaryImageUrl;
+    var priceGold = obj.priceGold;
+    var priceGoldAlt = obj.priceGoldAlt;
+    var priceRetail = obj.priceRetail;
+    var priceRetailAlt = obj.priceRetailAlt;
+
+    var currentCard = getTamplate();
+    var cardButton = currentCard.querySelector('.btn_cart');
+    var cardImage = currentCard.querySelector('.url--link').querySelector('img');
+    var cardPriceGold = currentCard.querySelector('.goldPrice');
+    var cardPriceRetail = currentCard.querySelector('.retailPrice');
+
+    cardButton.setAttribute('data-product-id', productId);
+    cardImage.src = changeFileName(imageUrl);
+    cardPriceGold.setAttribute('data-price', priceGold);
+    cardPriceGold.setAttribute('data-price-alt', priceGoldAlt);
+    cardPriceRetail.setAttribute('data-price', priceRetail);
+    cardPriceRetail.setAttribute('data-price-alt', priceRetailAlt);
+    cardPriceGold.textContent = setPrice(priceGold);
+    cardPriceRetail.textContent = setPrice(priceRetail);
+
+    return currentCard;
+  };
+
+  // добавление карточек
+  var renderCards = function (obj) {
+    for (var i = 0; obj.length > i; i++) {
+      productArea.append(specifyElement(obj[i]));
     }
   };
 
-  var getListener = function (evt) {
-    navLinksList.forEach(function (element) {
-      if (evt === 'add') {
-        element.addEventListener('click', onLinkClick);
-      } else {
-        element.removeEventListener('click', onLinkClick);
+  // обработка ошибок запроса
+  var getError = function (evt) {
+    // вывод ошибки в консоль
+    console.log(evt);
+
+    // вывод ошибки в браузер
+    // window.backend.renderError(evt);
+  };
+
+  // обработка успешного запроса
+  var getJsonAnsver = function (evt) {
+    tamlateContainer.style = 'display: none';
+
+    renderCards(evt);
+  };
+
+  // запрос сервера
+  window.backend.load(getJsonAnsver, getError);
+
+  // работа с количеством
+  var changeCount = function (elm) {
+    var arrow = elm;
+    var arrowContainer = arrow.closest('.stepper');
+    var countInput = arrowContainer.querySelector('input');
+
+    if (arrow.classList.contains('up')) {
+      countInput.value++;
+    } else if (arrow.classList.contains('down')) {
+      if (countInput.value > 1) {
+        countInput.value--;
       }
-    });
+    }
   };
 
-  navToggle.addEventListener('click', function () {
-    if (!navHeader.classList.contains('nav--active')) {
-      navHeader.classList.add('nav--active');
-      navToggle.classList.add('toggle__button--cross');
+  // смена цены
+  var priceChenge = function (elm) {
+    var linkClick = elm;
+    var cardWrapper = linkClick.closest('.product_horizontal');
+    var linklist = cardWrapper.querySelectorAll('.ng-binding');
+    var linkContainerList = cardWrapper.querySelectorAll('.unit--select');
 
-      getListener('add');
-    } else {
-      navHeader.classList.remove('nav--active');
-      navToggle.classList.remove('toggle__button--cross');
+    var goldPrice = cardWrapper.querySelector('.goldPrice');
+    var retailPrice = cardWrapper.querySelector('.retailPrice');
 
-      getListener();
+    if (!linkClick.closest('.unit--active')) {
+
+      linkContainerList.forEach(function (element) {
+        element.classList.remove('unit--active');
+      });
+      linkClick.closest('.unit--select').classList.add('unit--active');
+
+      if (linkClick === linklist[0]) {
+        goldPrice.textContent = setPrice(goldPrice.getAttribute('data-price'));
+        retailPrice.textContent = setPrice(retailPrice.getAttribute('data-price'));
+      } else if (linkClick === linklist[1]) {
+        goldPrice.textContent = setPrice(goldPrice.getAttribute('data-price-alt'));
+        retailPrice.textContent = setPrice(retailPrice.getAttribute('data-price-alt'));
+      }
     }
-  });
-} catch (e) {
-  throw Error('Ошибка ' + e.name + ':' + e.message + '\n' + e.stack);
-}
+
+  };
+
+  // обработка событий
+  var cardClickHandler = function (evt) {
+    if (evt.target.classList.contains('stepper-arrow')) {
+      changeCount(evt.target);
+    }
+    if (evt.target.classList.contains('ng-binding')) {
+      priceChenge(evt.target);
+    }
+  };
+
+  document.addEventListener('click', cardClickHandler);
+})();
+
